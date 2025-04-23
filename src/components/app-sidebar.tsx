23845501238be81e609974aecb2d3705942be99b
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-04-24 03:00:00 +0900

    style: 사이드바 구조 변경

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -2,7 +2,13 @@
 
 import * as React from 'react';
 import { usePathname } from 'next/navigation';
-import { BookOpen, Bot, Settings2, SquareTerminal } from 'lucide-react';
+import {
+  Users,
+  Newspaper,
+  FolderSearch,
+  Boxes,
+  CircleFadingPlus,
+} from 'lucide-react';
 
 import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
@@ -10,7 +16,6 @@ import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
-  SidebarRail,
 } from '@/components/ui/sidebar';
 
 const baseData = {
@@ -22,27 +27,27 @@ const baseData = {
   navMain: [
     {
       title: '사용자',
-      url: '/',
-      icon: SquareTerminal,
+      url: '/users',
+      icon: Users,
       items: [
-        { title: '연명부', url: '/members' },
-        { title: '휴가', url: '/vacations' },
-        { title: '자리배치도', url: '/seating' },
+        // { title: '연명부', url: '/members' },
+        { title: '휴가', url: '/users/leaves' },
+        { title: '자리배치도', url: '/users/seats' },
       ],
     },
     {
       title: '업무',
-      url: '/tasks',
-      icon: Bot,
+      url: '/reports',
+      icon: Newspaper,
       items: [
-        { title: '집행부 업무 보고', url: '/tasks/report' },
-        { title: '일일 업무 보고', url: '/tasks/daily' },
+        { title: '일일 업무 보고', url: '/reports/daily' },
+        { title: '집행부 업무 보고', url: '/reports/weekly' },
       ],
     },
     {
       title: '연구',
       url: '/researches',
-      icon: BookOpen,
+      icon: FolderSearch,
       items: [
         { title: 'RSS 공고', url: '/researches/rss' },
         { title: '연구 & 프로젝트', url: '/researches/projects' },
@@ -50,23 +55,23 @@ const baseData = {
     },
     {
       title: '물품',
-      url: '/items',
-      icon: Settings2,
+      url: '/goods',
+      icon: Boxes,
       items: [
-        { title: '물자 관리', url: '/items/manage' },
-        { title: '보안 컴퓨터 관리', url: '/items/security' },
+        { title: '물자 관리', url: '/goods/management' },
+        { title: '보안 컴퓨터 관리', url: '/items/computers' },
       ],
     },
     {
       title: '기타',
       url: '/etc',
-      icon: Settings2,
+      icon: CircleFadingPlus,
       items: [{ title: '정보 게시판', url: '/etc/board' }],
     },
   ],
 };
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+export function AppSidebar() {
   const pathname = usePathname();
 
   if (pathname.startsWith('/researches/projects/')) {
@@ -74,39 +79,27 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   }
 
   const navMain = baseData.navMain.map((group) => {
-    const isActiveGroup =
-      pathname === group.url ||
-      pathname.startsWith(`${group.url}/`) ||
-      group.items?.some(
-        (subItem) =>
-          pathname === subItem.url || pathname.startsWith(`${subItem.url}/`),
-      );
-
     const updatedItems = group.items?.map((subItem) => ({
       ...subItem,
-      isActive:
-        pathname === subItem.url || pathname.startsWith(`${subItem.url}/`),
+      isActive: true,
     }));
 
     return {
       ...group,
-      isActive: isActiveGroup,
+      isActive: true,
       items: updatedItems,
     };
   });
 
   return (
-    <div className="flex h-[calc(100vh-80px)]">
-      <Sidebar {...props}>
-        <div className="flex h-full w-full flex-col justify-between overflow-hidden">
-          <SidebarContent>
-            <NavMain groupLabel="사용자" items={navMain} />
-          </SidebarContent>
-          <SidebarFooter>
-            <NavUser user={baseData.user} />
-          </SidebarFooter>
-        </div>
-        <SidebarRail />
+    <div className="flex h-[calc(100vh-65px)] overflow-hidden">
+      <Sidebar className="flex h-full w-[16rem] flex-col border-r">
+        <SidebarContent className="flex-1 overflow-auto">
+          <NavMain items={navMain} />
+        </SidebarContent>
+        <SidebarFooter className="shrink-0 border-t bg-white px-4 py-3">
+          <NavUser user={baseData.user} />
+        </SidebarFooter>
       </Sidebar>
     </div>
   );
