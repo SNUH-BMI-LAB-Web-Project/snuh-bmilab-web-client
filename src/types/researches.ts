7adc23c1eeeb584f0bda09b7a18236a511c74c27
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-04-24 05:43:29 +0900

    chore: 프로젝트 미팅 관련 더미, 타입 추가 (임시)

--- src/types/researches.ts
+++ src/types/researches.ts
@@ -74,6 +74,31 @@ export interface ProjectFile {
   type: string;
 }
 
+// =================== PROJECT ===================
+export interface Meeting {
+  id: string;
+  projectId: string;
+  title: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  type: string;
+  participants: string[];
+  summary: string;
+  content: string;
+  createdAt: string;
+  createdBy: string;
+  comments: MeetingComment[];
+}
+
+export interface MeetingComment {
+  id: string;
+  meetingId: string;
+  userId: string;
+  content: string;
+  createdAt: string;
+}
+
 // =================== RSS ===================
 export interface Rss {
   title: string;
