c4a3507ce3cfac771b89ad70185a2cc38527cd1d
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-04-24 02:58:16 +0900

    feat: 유저 호버시 팝오버창 구현

---
+++ src/components/common/user-popover.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@/components/ui/popover';
+import { User } from '@/types/researches';
+import Image from 'next/image';
+
+export default function UserPopover({ user }: { user: User }) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <span className="cursor-pointer hover:text-black">{user.name}</span>
+      </PopoverTrigger>
+      <PopoverContent side="top" align="center" className="w-72 text-sm">
+        <div className="text-muted-foreground flex items-center gap-3 text-sm">
+          <Image
+            src={user?.profileImageUrl || '/default-avatar.png'}
+            alt={user?.name || '사용자 프로필'}
+            width={40}
+            height={40}
+            className="h-10 w-10 rounded-full object-cover"
+          />
+          <div className="flex flex-col">
+            <div className="font-medium text-black">{user?.name}</div>
+            <div className="text-xs">
+              {user?.department} · {user?.email}
+            </div>
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+}
