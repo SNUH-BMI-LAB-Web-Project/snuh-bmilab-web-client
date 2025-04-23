9b8b8c4ce8299e650bb8f28db8f75c0f34039011
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-04-24 02:57:43 +0900

    feat: 상태에 따른 뱃지 색상 유틸함수 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import type { ResearchStatus } from '@/types/researches';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -18,3 +19,18 @@ export function formatDateTime(dateStr: string): string {
 
   return `${yyyy}년 ${mm}월 ${dd}일 (${day}) ${hh}:${min}`;
 }
+
+export const getStatusColor = (status: ResearchStatus) => {
+  switch (status) {
+    case '진행 전':
+      return 'bg-blue-100 text-blue-800 hover:bg-blue-100';
+    case '진행 중':
+      return 'bg-green-100 text-green-800 hover:bg-green-100';
+    case '진행 종료':
+      return 'bg-gray-100 text-gray-800 hover:bg-gray-100';
+    case '진행 대기':
+      return 'bg-yellow-100 text-yellow-800 hover:bg-yellow-100';
+    default:
+      return '';
+  }
+};
