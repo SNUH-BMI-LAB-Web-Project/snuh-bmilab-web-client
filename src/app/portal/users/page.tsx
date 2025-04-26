1378e056e171e2882595418464476bcee24063b9
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-04-27 07:31:57 +0900

    feat: 미완료 기능 에러처리

---
+++ src/app/portal/users/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { users } from '@/data/users';
+import UserInfoCard from '@/components/users/user-info-card';
+
+export default function UsersPage() {
+  return (
+    <div className="mb-8 flex flex-col gap-8">
+      <h1 className="text-3xl font-bold">연명부</h1>
+
+      {/* 유저 카드 리스트 */}
+      <div className="grid w-full max-w-7xl grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {users.map((user) => (
+          <UserInfoCard key={user.userId} user={user} />
+        ))}
+      </div>
+    </div>
+  );
+}
