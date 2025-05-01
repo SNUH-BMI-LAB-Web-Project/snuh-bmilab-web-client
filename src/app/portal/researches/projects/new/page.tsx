9f9035b7ef638e199662ee378b67813635bbb0b0
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-05-01 23:59:42 +0900

    feat: 프로젝트 생성 api 연결, 에러처리 미완

--- src/app/portal/researches/projects/new/page.tsx
+++ src/app/portal/researches/projects/new/page.tsx
@@ -4,20 +4,41 @@ import { useRouter } from 'next/navigation';
 import { ProjectForm } from '@/components/researches/projects/project-form';
 import { Button } from '@/components/ui/button';
 import { ArrowLeft } from 'lucide-react';
-import { Project } from '@/types/project';
+import { ProjectApi } from '@/generated-api/apis/ProjectApi';
+import { Configuration } from '@/generated-api/runtime';
+import { useAuthStore } from '@/store/auth-store';
+import { ProjectRequest } from '@/generated-api';
+
+const projectApi = new ProjectApi(
+  new Configuration({
+    accessToken: async () => useAuthStore.getState().accessToken ?? '',
+  }),
+);
 
 export default function NewProject() {
   const router = useRouter();
 
-  // 프로젝트 생성 처리 (실제 구현에서는 API 호출 필요)
-  const handleSubmit = (data: Project) => {
-    console.log('새 프로젝트 데이터:', data);
-    // 여기에 실제 생성 로직 구현
-    router.push('/');
+  // TODO: 토스트 처리
+  const handleCreate = async (data: ProjectRequest, newFiles: File[]) => {
+    try {
+      console.log('폼 제출 데이터:', data);
+
+      await projectApi.createNewProject({
+        request: data,
+        files: newFiles,
+      });
+
+      alert('프로젝트가 성공적으로 등록되었습니다!');
+      router.push('/portal/researches/projects');
+    } catch (error) {
+      console.error('프로젝트 생성 실패:', error);
+      alert('프로젝트 등록에 실패했습니다.');
+    }
   };
 
   return (
     <div className="flex flex-col px-30">
+      {/* 헤더 */}
       <div className="mb-6 flex items-center">
         <Button
           variant="ghost"
@@ -30,7 +51,8 @@ export default function NewProject() {
         <h1 className="text-3xl font-bold">연구 & 프로젝트 등록</h1>
       </div>
 
-      <ProjectForm onSubmit={handleSubmit} />
+      {/* 등록 폼 */}
+      <ProjectForm onCreate={handleCreate} />
     </div>
   );
 }
