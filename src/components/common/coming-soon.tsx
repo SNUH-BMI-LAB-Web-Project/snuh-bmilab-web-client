4173dbf0f41594e1f5f8d54fdbc7786c01156efc
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-05-01 23:53:21 +0900

    style: 컴포넌트 적용 영역 너비 전체로 갖도록

--- src/components/common/coming-soon.tsx
+++ src/components/common/coming-soon.tsx
@@ -2,7 +2,7 @@
 
 export default function ComingSoon() {
   return (
-    <div className="flex flex-col items-center justify-center py-60">
+    <div className="flex w-full flex-col items-center justify-center py-60">
       <h1 className="mb-4 animate-pulse text-5xl font-bold tracking-widest">
         Coming Soon
       </h1>
