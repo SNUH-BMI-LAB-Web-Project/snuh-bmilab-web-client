d157b45e945b46d9aa76a4571ab115fdedc210bb
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-07-04 16:14:52 +0900

    !HOTFIX: 마크다운 에디터 컴포넌트 구현

---
+++ src/components/portal/researches/projects/markdown-editor.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import React, { useRef, useEffect } from 'react';
+import { Editor } from '@toast-ui/react-editor';
+
+interface MarkdownEditorProps {
+  content: string;
+  setContent: (value: string) => void;
+}
+
+export default function MarkdownEditor({
+  content,
+  setContent,
+}: MarkdownEditorProps) {
+  const editorRef = useRef<Editor>(null);
+
+  useEffect(() => {
+    const editorInstance = editorRef.current?.getInstance();
+
+    if (!editorInstance) return undefined;
+
+    const handleChange = () => {
+      const markdown = editorInstance.getMarkdown();
+      setContent(markdown);
+    };
+
+    editorInstance.on('change', handleChange);
+
+    return () => {
+      editorInstance.off('change', handleChange);
+    };
+  }, [setContent]);
+
+  return (
+    <Editor
+      ref={editorRef}
+      initialValue={
+        content?.trim() === '' ? '여기에 내용을 입력해 주세요...' : content
+      }
+      placeholder=" "
+      height="400px"
+      initialEditType="markdown"
+      previewStyle="vertical"
+      hideModeSwitch
+      useCommandShortcut
+      toolbarItems={[
+        ['heading', 'bold', 'italic', 'strike'],
+        ['hr', 'quote'],
+        ['ul', 'ol', 'task'],
+        ['code', 'codeblock'],
+      ]}
+    />
+  );
+}
