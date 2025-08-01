f01ecf643e3306adea417043f313042f75c988e6
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2025-08-01 15:42:15 +0900

    !HOTFIX: created At -> date로 렌더링 수정

--- src/components/system/reports/admin-report-feed.tsx
+++ src/components/system/reports/admin-report-feed.tsx
@@ -19,6 +19,7 @@ import {
   ReportSummary,
 } from '@/generated-api';
 import { getApiConfig } from '@/lib/config';
+import { formatDateTimeVer2 } from '@/lib/utils';
 
 const reportApi = new AdminReportApi(getApiConfig());
 
@@ -81,10 +82,7 @@ export function AdminReportFeed({
                 </div>
                 <div className="flex items-center gap-2">
                   <p className="text-right text-xs font-medium">
-                    {report.createdAt &&
-                      format(report.createdAt, 'yyyy년 MM월 dd일', {
-                        locale: ko,
-                      })}
+                    {formatDateTimeVer2(report.date!)}
                   </p>
                 </div>
               </div>
