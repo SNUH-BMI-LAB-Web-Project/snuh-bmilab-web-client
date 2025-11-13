ab9068c47f79dc0c28faf8964a198ebab3643f87
hyeonjin6530 <cola1876@naver.com>
2025-11-13 12:47:19 +0900

    fix: PapersTab이 없어서 발생하는 error fix

---
+++ src/components/portal/researches/tasks/detailTabs/SubTabs/PapersTab.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import ComingSoon from '@/components/common/coming-soon';
+
+export default function PapersTab() {
+  return (
+    <div className="w-full">
+      <ComingSoon />
+    </div>
+  );
+}
