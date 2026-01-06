8335d17a966a31a8ffa1b2cd8adf4feecb0c8b71
pkm021118 <pkm021117@gmail.com>
2026-01-07 07:03:01 +0900

    [feat]: 연구성과 페이지 api 연결

--- next.config.ts
+++ next.config.ts
@@ -3,24 +3,23 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  eslint: {
-    // CI/production builds should not fail on lint rules; run `yarn lint` separately.
-    ignoreDuringBuilds: true,
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
   images: {
-    domains: [
-      'github.com',
-      'bmi-bucket.s3.ap-northeast-2.amazonaws.com', // S3 이미지 도메인 추가
-      'bmilab-bucket.s3.ap-northeast-2.amazonaws.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'bmi-bucket.s3.ap-northeast-2.amazonaws.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'bmilab-bucket.s3.ap-northeast-2.amazonaws.com',
+        pathname: '/**',
+      },
     ],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; img-src 'self' data: https:;",
